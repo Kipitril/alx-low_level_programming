2b5ed1d3032ec6ed4aa8cc84709026289eb675ba
Kipitril <hezkipkorir@gmail.com>
2023-03-18 05:19:41 -0700

    c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,18 +2,16 @@
 
 /**
  * main - Prints numbers from 0 to 9.
- *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
-
 int main(void)
 {
-        int i;
+	int num;
 
-        for (i = 48; i, 58; i++)
-        {
-                putchar (i);
-        }
-        putchar ('\n');
-        return (0);
+	for(num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	
+	putchar('\n');
+
+	return (0);
 }
